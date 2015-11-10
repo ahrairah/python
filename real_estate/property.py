7d8d3b3b6f5327d9040fa63ca9fa6e1c89b8f1ff
Tony Meyer <Tony.Meyer@outlook.de>
2015-11-10 14:39:13 +0100

    filled class Apartment

--- real_estate/property.py
+++ real_estate/property.py
@@ -16,12 +16,38 @@ class Property:
             return dict(square_feet=input("Enter the square feet: "),
                         beds=input("Enter number of bedrooms: "),
                         baths=input("Enter number of baths: "))
-            prompt_init = staticmethod(prompt_init)
+
+        prompt_init = staticmethod(prompt_init)
 
 
 class House:
     pass
 
 
-class Apartment:
-    pass
+class Apartment(Property):
+    valid_laundries = ("coins", "ensuite", "none")
+    valid_balconies = ("yes", "no", "solarium")
+
+    def __init__(self, balcony='', laundry='', **kwargs):
+        super().__init__(**kwargs)
+        self.balcony = balcony
+        self.laundry = laundry
+
+    def display(self):
+        super().display()
+        print("APARTMENT DETAILS")
+        print("laundry: {}".format(self.laundry))
+        print("has balcony: {}".format(self.balcony))
+
+        parent_init = Property.prompt_init()
+        laundry = ''
+        while laundry.lower() not in Apartment.valid_laundries:
+            laundry = input(
+                "What laundry facilities does the property have? ({})".format(", ".join(Apartment.valid_laundries)))
+        balcony = ''
+        while balcony.lower() not in Apartment.valid_balconies:
+            balcony = input("Does the property ({}) have a balcony".format(", ".join(Apartment.valid_balconies)))
+        parent_init.update({"laundry": laundry, "balcony": balcony})
+        return parent_init
+
+    prompt_init = staticmethod(Property.prompt_init())
