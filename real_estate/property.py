fc42bd91e73b5fe2b2cecc6fa3e1848a14f525ca
Tony Meyer <Tony.Meyer@outlook.de>
2015-11-24 15:37:31 +0100

    Kleinere Anpassungen.

--- real_estate/property.py
+++ real_estate/property.py
@@ -12,12 +12,12 @@ class Property:
         print("bedrooms: [].".format(self.num_bedrooms))
         print("bathrooms: {}.".format(self.num_bathrooms))
 
-        def prompt_init():
-            return dict(square_feet=input("Enter the square feet: "),
-                        beds=input("Enter number of bedrooms: "),
-                        baths=input("Enter number of baths: "))
+    def prompt_init():
+        return dict(square_feet=input("Enter the square feet: "),
+                    beds=input("Enter number of bedrooms: "),
+                    baths=input("Enter number of baths: "))
 
-        prompt_init = staticmethod(prompt_init)
+    prompt_init = staticmethod(prompt_init)
 
 
 class House:
